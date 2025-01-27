ffdb5ee01642db83f3f20e6c63a836cf613b513f
Emanuelly Valenga <emanuellyvalenga.dev@gmail.com>
2025-01-27 16:47:14 -0300

    Começando um projeto em Laravel na versão 9 Adicionando Controller de Series Ajustando o arquivo de routes Manipulando as views com components

---
+++ laravel-crie-aplicacoes-web-em-php/criando-aplicacao-mvc/controle-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index (Request $request)
+    {
+        // return redirect('pagina');
+        $series = [
+            'Suits',
+            'Cobra Kai',
+            'The Office'
+        ];
+
+    return view('series.index')->with('series', $series);
+    }
+
+    public function create ()
+    {
+        return view('series.create');
+    }
+}
---
+++ laravel-crie-aplicacoes-web-em-php/criando-aplicacao-mvc/controle-series/resources/views/series/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout title="Séries">
+    <a href="/series/criar">Adicionar série</a>
+
+    <ul>
+        @foreach ($series as $serie)
+        <li>{{ $serie }}</li>
+        @endforeach
+    </ul>
+
+
+</x-layout>
