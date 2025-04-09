a1481a9bb93162326c6b2ff5a4b8d913b2f07077
Emanuelly Valenga <emanuellyvalenga.dev@gmail.com>
2025-04-09 17:36:45 -0300

    Aprofundando mais sobre herança, utilizando construct, parent, protected

--- curso-avancando-com-OO-heranca-polimorfismo-interfaces/banco.php
+++ curso-avancando-com-OO-heranca-polimorfismo-interfaces/banco.php
@@ -1,11 +1,13 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
-$primeiraConta = new Conta($vinicius);
+$endereco = new Endereco('Curitiba', 'Cachoeira', 'Travessa Schultz', '288');
+$emanuelly = new Titular(new CPF('123.456.789-10'), 'Emanuelly', $endereco);
+$primeiraConta = new Conta($emanuelly);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
@@ -13,10 +15,11 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
-$segundaConta = new Conta($patricia);
+$isabelle = new Titular(new CPF('698.549.548-10'), 'Isabelle', $endereco);
+$segundaConta = new Conta($isabelle);
 var_dump($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+$outroEndereco = new Endereco('A', 'B', 'c', '111');
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
